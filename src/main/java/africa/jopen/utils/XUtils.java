5c813df769f7fa3d23750c8e9166f4dc6e73f084
Kinsley Kajiva <kajivakinsley@gmail.com>
2023-06-02 00:14:33 +0200

    update pull to keep up
    Signed-off-by: Kinsley Kajiva <kajivakinsley@gmail.com>

--- src/main/java/africa/jopen/utils/XUtils.java
+++ src/main/java/africa/jopen/utils/XUtils.java
@@ -1,19 +1,44 @@
 package africa.jopen.utils;
 
 import com.google.common.flogger.FluentLogger;
+import com.sun.jna.Platform;
+import com.sun.jna.platform.win32.Kernel32;
 import io.helidon.common.http.Http;
 import io.helidon.webserver.ServerResponse;
 import jakarta.json.*;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.Objects;
 import java.util.TimeZone;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 public class XUtils {
 	private XUtils() {
 	}
-	
+	public static boolean isFilePath(String path) {
+		try {
+			// Attempt to resolve the path
+			Paths.get(path);
+			return true; // If no exception, it's a valid file path
+		} catch (InvalidPathException e) {
+			return false;
+		}
+	}
+
+	public static boolean isURL(String path) {
+		try {
+			new URL(path); // Attempt to parse the string as a URL
+			return true; // If no exception, it's a valid URL
+		} catch (MalformedURLException e) {
+			return false;
+		}
+	}
 	private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 	private static final JsonBuilderFactory JSON = Json.createBuilderFactory(Collections.emptyMap());
 	
@@ -128,6 +153,69 @@ public class XUtils {
 				                          .build();
 		response.send(returnObject);
 	}
-	
-	
+
+	public static final class GStreamerUtils{
+
+		private GStreamerUtils(){}
+
+		/**
+		 * Configures paths to the GStreamer libraries. On Windows queries various
+		 * GStreamer environment variables, and then sets up the PATH environment
+		 * variable. On macOS, adds the location to jna.library.path (macOS binaries
+		 * link to each other). On both, the gstreamer.path system property can be
+		 * used to override. On Linux, assumes GStreamer is in the path already.
+		 */
+		public static void configurePaths() {
+			if (Platform.isWindows()) {
+				String gstPath = System.getProperty("gstreamer.path", findWindowsLocation());
+				if (!gstPath.isEmpty()) {
+					String systemPath = System.getenv("PATH");
+					if (systemPath == null || systemPath.trim().isEmpty()) {
+						Kernel32.INSTANCE.SetEnvironmentVariable("PATH", gstPath);
+					} else {
+						Kernel32.INSTANCE.SetEnvironmentVariable("PATH", gstPath
+								+ File.pathSeparator + systemPath);
+					}
+				}
+			} else if (Platform.isMac()) {
+				String gstPath = System.getProperty("gstreamer.path",
+						"/Library/Frameworks/GStreamer.framework/Libraries/");
+				if (!gstPath.isEmpty()) {
+					String jnaPath = System.getProperty("jna.library.path", "").trim();
+					if (jnaPath.isEmpty()) {
+						System.setProperty("jna.library.path", gstPath);
+					} else {
+						System.setProperty("jna.library.path", jnaPath + File.pathSeparator + gstPath);
+					}
+				}
+
+			}
+		}
+
+		/**
+		 * Query over a stream of possible environment variables for GStreamer
+		 * location, filtering on the first non-null result, and adding \bin\ to the
+		 * value.
+		 *
+		 * @return location or empty string
+		 */
+		static String findWindowsLocation() {
+			if (Platform.is64Bit()) {
+				return Stream.of("GSTREAMER_1_0_ROOT_MSVC_X86_64",
+								"GSTREAMER_1_0_ROOT_MINGW_X86_64",
+								"GSTREAMER_1_0_ROOT_X86_64")
+						.map(System::getenv)
+						.filter(p -> p != null)
+						.map(p -> p.endsWith("\\") ? p + "bin\\" : p + "\\bin\\")
+						.findFirst().orElse("");
+			} else {
+				return "";
+			}
+		}
+
+
+
+	}
+
+
 }
