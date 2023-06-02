09ec4909626a2bfc13eb0c1c53713fbc5be88338
Kinsley Kajiva <kajivakinsley@gmail.com>
2023-06-02 18:50:44 +0200

    update
    Signed-off-by: Kinsley Kajiva <kajivakinsley@gmail.com>

--- src/main/java/africa/jopen/app/App.java
+++ src/main/java/africa/jopen/app/App.java
@@ -1,16 +1,68 @@
 package africa.jopen.app;
 
 
+import africa.jopen.models.configs.main.MainConfigModel;
+import africa.jopen.utils.XUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.flogger.FluentLogger;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.io.File;
+import java.nio.file.Paths;
+
 
 @QuarkusMain
 public class App {
+
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
     public static void main(String... args) {
-        System.out.println("Running main method");
-        Quarkus.run(Main.class,args);
-        //Quarkus.run(args);
+        logger.atInfo().log("Server Starting");
+        Config configNameConfig = ConfigProvider.getConfig();
+        final String configName = configNameConfig.getValue("app.config.folderName", String.class);
+
+        XUtils.BASE_APP_LOCATION_PATH = Paths.get("").toAbsolutePath().toString();
+
+        logger.atInfo().log("App running in folder path " + XUtils.BASE_APP_LOCATION_PATH);
+        logger.atInfo().log("App running in config folder path " + XUtils.BASE_APP_LOCATION_PATH + "/" + configName);
+
+        File file;
+        try {
+
+            file = new File(XUtils.BASE_APP_LOCATION_PATH + "/" + configName);
+            if (!file.exists()) {
+                logger.atSevere().log("Configs not found");
+                System.exit(1);
+            }
+        } catch (Exception e) {
+
+            logger.atSevere().withCause(e).log("Configs not found");
+        }
+
+        try {
+            file = new File(XUtils.BASE_APP_LOCATION_PATH + "/" + configName + "/mainConfig.json");
+            if (!file.exists()) {
+                logger.atSevere().log("mainConfig .json not found");
+                System.exit(1);
+            }
+            ObjectMapper objectMapper = new ObjectMapper();
+            XUtils.MAIN_CONFIG_MODEL = objectMapper.readValue(file, MainConfigModel.class);
+        } catch (Exception e) {
+            logger.atSevere().withCause(e).log("Failed to process");
+        }
+
+        System.setProperty("quarkus.application.name", XUtils.MAIN_CONFIG_MODEL.serverName());
+        System.setProperty("quarkus.http.port", String.valueOf(XUtils.MAIN_CONFIG_MODEL.serverPort()));
+        System.setProperty("quarkus.http.ssl-port", String.valueOf(XUtils.MAIN_CONFIG_MODEL.serverSSLPort()));
+        System.setProperty("quarkus.application.version", XUtils.MAIN_CONFIG_MODEL.serverVersion());
+        logger.atInfo().log("Server configs loaded successfully !");
+        Quarkus.run(Main.class, args);
     }
 
 
--- src/main/java/africa/jopen/app/Main.java
+++ src/main/java/africa/jopen/app/Main.java
@@ -25,36 +25,9 @@ public class Main implements QuarkusApplication {
 
     // @Override
     public int run(String... args) throws Exception {
-        XUtils.BASE_APP_LOCATION_PATH = Paths.get("").toAbsolutePath().toString();
-
         logger.atInfo().log("Server Started");
-        logger.atInfo().log("App running in folder path " + XUtils.BASE_APP_LOCATION_PATH);
-        logger.atInfo().log("App running in config folder path " + XUtils.BASE_APP_LOCATION_PATH + "/" + configName);
 
-        File file;
-        try {
 
-            file = new File(XUtils.BASE_APP_LOCATION_PATH + "/" + configName);
-            if (!file.exists()) {
-                logger.atSevere().log("Configs not found");
-                System.exit(1);
-            }
-        } catch (Exception e) {
-
-            logger.atSevere().withCause(e).log("Configs not found");
-        }
-
-        try {
-            file = new File(XUtils.BASE_APP_LOCATION_PATH + "/" + configName + "/mainConfig.json");
-            if (!file.exists()) {
-                logger.atSevere().log("mainConfig .json not found");
-                System.exit(1);
-            }
-            ObjectMapper objectMapper = new ObjectMapper();
-            XUtils.MAIN_CONFIG_MODEL = objectMapper.readValue(file, MainConfigModel.class);
-        } catch (Exception e) {
-            logger.atSevere().withCause(e).log("Failed to process");
-        }
 
 
         Quarkus.waitForExit();
