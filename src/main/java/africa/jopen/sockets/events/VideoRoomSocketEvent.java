67ed200b2a87835ee4814bfa032356de0b5c11b4
Kinsley Kajiva <kajivakinsley@gmail.com>
2023-08-29 11:15:46 +0200

    Refactor `handleVideoRoomRequest` to improve code readability
    Cleaned up the `handleVideoRoomRequest` method in VideoRoomSocketEvent. The method was cluttered with many nested conditions and repetitive error handling code. The redundant code was extracted to separate, more compact methods - `handleJoinRoomRequest`, `handleCreateRoomRequest`, `handleInvalidRequest`, `handleValidationError` and `handleAuthenticationError`. This refactoring improves the maintainability and readability of the code.
    
    Signed-off-by: Kinsley Kajiva <kajivakinsley@gmail.com>

--- src/main/java/africa/jopen/sockets/events/VideoRoomSocketEvent.java
+++ src/main/java/africa/jopen/sockets/events/VideoRoomSocketEvent.java
@@ -21,76 +21,99 @@ import static africa.jopen.sockets.ClientWebSocket.rememberResponse;
 public class VideoRoomSocketEvent {
     private static final FluentLogger logger = FluentLogger.forEnclosingClass();
     private static final ConnectionsManager connectionsManager = ConnectionsManager.getInstance();
+    
     public static void handleVideoRoomRequest(Client clientObject, JSONObject messageObject, JSONObject response) {
         final String requestType = messageObject.getString(Requests.REQUEST_TYPE);
         response.put("history", messageObject);
         
-        
         switch (requestType) {
-            case Requests.REMEMBER -> response = rememberResponse(connectionsManager,clientObject);
-            case "joinRoom" -> {
-                if (!messageObject.has("password")) {
-                    response = XUtils.buildJsonErrorResponse(Requests.SERVER_BAD_REQUEST,  Events.EVENT_TYPE,  Events.VALIDATION_ERROR_EVENT,"password is required", response);
-                    broadcast(clientObject, response.toString());
-                    return;
-                }
-                if (!messageObject.has("clientID")) {
-                    response = XUtils.buildJsonErrorResponse(Requests.SERVER_BAD_REQUEST,  Events.EVENT_TYPE,  Events.VALIDATION_ERROR_EVENT,"clientID is required", response);
-                    broadcast(clientObject, response.toString());
-                    return;
-                }
-                var room = new PostJoinRoom(
-                        messageObject.getString("roomID"),
-                        messageObject.getString("password"),
-                        messageObject.getString("clientID")
-                );
-                Optional<RoomModel> roomModelOptional = connectionsManager.getRoomById(room.roomID());
-                if (roomModelOptional.isEmpty()) {
-                    response = XUtils.buildJsonErrorResponse(Requests.SERVER_BAD_REQUEST,  Events.EVENT_TYPE,  Events.VALIDATION_ERROR_EVENT,"Invalid room ID!", response);
-                    broadcast(clientObject, response.toString());
-                    return;
-                }
-                RoomModel roomModel = roomModelOptional.get();
-                if (!roomModel.getPassword().equals(room.password())) {
-                    response = XUtils.buildJsonErrorResponse(Requests.SERVER_BAD_REQUEST,  Events.EVENT_TYPE, "authentication","Room authentication failed! Access rejected.", response);
-                    broadcast(clientObject, response.toString());
-                    return;
-                }
-                VideoRoomController.joinRoom(connectionsManager, room, roomModel);
-                response = XUtils.buildJsonSuccessResponse(Requests.OK_RESPONSE, "eventType", "joinRoom",
-                        "Added to room", response);
-            }
-            case "createRoom" -> {
-                if (!messageObject.has("pin")) {
-                    response = XUtils.buildJsonErrorResponse(Requests.SERVER_BAD_REQUEST,  Events.EVENT_TYPE,  Events.VALIDATION_ERROR_EVENT,"Pin is required", response);
-                    broadcast(clientObject, response.toString());
-                    return;
-                }
-                if (!messageObject.has("password")) {
-                    response = XUtils.buildJsonErrorResponse(Requests.SERVER_BAD_REQUEST,  Events.EVENT_TYPE,  Events.VALIDATION_ERROR_EVENT,"password is required", response);
-                    broadcast(clientObject, response.toString());
-                    return;
-                }
-                if (!messageObject.has("creatorClientID")) {
-                    response = XUtils.buildJsonErrorResponse(Requests.SERVER_BAD_REQUEST,  Events.EVENT_TYPE,  Events.VALIDATION_ERROR_EVENT,"creatorClientID is required", response);
-                    broadcast(clientObject, response.toString());
-                    return;
-                }
-                var post = new PostCreateRoom(messageObject.getString("roomName"),
-                        messageObject.getString("roomDescription"),
-                        messageObject.getString("pin"),
-                        messageObject.getString("password"),
-                        messageObject.getString("creatorClientID")
-                );
-                Map<String, Object> resultRoom = VideoRoomController.createRoom(connectionsManager, post, clientObject);
-                response.put("room", resultRoom);
-                response = XUtils.buildJsonSuccessResponse(Requests.OK_RESPONSE,  Events.EVENT_TYPE, "createRoom","Room created successfully", response);
-            }
-            default -> {
-                response.put("clientID", clientObject.getClientID());
-                response = XUtils.buildJsonErrorResponse(Requests.SERVER_BAD_REQUEST,  Events.EVENT_TYPE,  Events.VALIDATION_ERROR_EVENT,"Invalid request type for VIDEO_ROOM feature", response);
-            }
+            case Requests.REMEMBER -> response = rememberResponse(connectionsManager, clientObject);
+            case "joinRoom" -> response = handleJoinRoomRequest(messageObject, clientObject, response);
+            case "createRoom" -> response = handleCreateRoomRequest(messageObject, clientObject, response);
+            default -> response = handleInvalidRequest(response, clientObject);
         }
+        
+        broadcast(clientObject, response.toString());
+    }
+    
+    
+    private static JSONObject handleJoinRoomRequest(JSONObject messageObject, Client clientObject, JSONObject response) {
+        if (!messageObject.has("password")) {
+            handleValidationError("password is required", response, clientObject);
+            return messageObject;
+        }
+        if (!messageObject.has("clientID")) {
+            handleValidationError("clientID is required", response, clientObject);
+            return messageObject;
+        }
+        
+        var room = new PostJoinRoom(
+                messageObject.getString("roomID"),
+                messageObject.getString("password"),
+                messageObject.getString("clientID")
+        );
+        
+        Optional<RoomModel> roomModelOptional = connectionsManager.getRoomById(room.roomID());
+        if (roomModelOptional.isEmpty()) {
+            handleValidationError("Invalid room ID!", response, clientObject);
+            return messageObject;
+        }
+        
+        RoomModel roomModel = roomModelOptional.get();
+        if (!roomModel.getPassword().equals(room.password())) {
+            handleAuthenticationError("Room authentication failed! Access rejected.", response, clientObject);
+            return messageObject;
+        }
+        
+        VideoRoomController.joinRoom(connectionsManager, room, roomModel);
+        response = XUtils.buildJsonSuccessResponse(Requests.OK_RESPONSE, "eventType", "joinRoom",
+                "Added to room", response);
+        return response;
+    }
+    
+    
+    private static JSONObject handleCreateRoomRequest(JSONObject messageObject, Client clientObject, JSONObject response) {
+        if (!messageObject.has("pin")) {
+            handleValidationError("Pin is required", response, clientObject);
+            return new JSONObject();
+        }
+        if (!messageObject.has("password")) {
+            handleValidationError("password is required", response, clientObject);
+            return new JSONObject();
+        }
+        if (!messageObject.has("creatorClientID")) {
+            handleValidationError("creatorClientID is required", response, clientObject);
+            return new JSONObject();
+        }
+        
+        var post = new PostCreateRoom(
+                messageObject.getString("roomName"),
+                messageObject.getString("roomDescription"),
+                messageObject.getString("pin"),
+                messageObject.getString("password"),
+                messageObject.getString("creatorClientID")
+        );
+        
+        Map<String, Object> resultRoom = VideoRoomController.createRoom(connectionsManager, post, clientObject);
+        response.put("room", resultRoom);
+        response = XUtils.buildJsonSuccessResponse(Requests.OK_RESPONSE, Events.EVENT_TYPE, "createRoom", "Room created successfully", response);
+        return response;
+    }
+    
+    private static JSONObject handleInvalidRequest(JSONObject response, Client clientObject) {
+        response.put("clientID", clientObject.getClientID());
+        response = XUtils.buildJsonErrorResponse(Requests.SERVER_BAD_REQUEST, Events.EVENT_TYPE, Events.VALIDATION_ERROR_EVENT, "Invalid request type for VIDEO_ROOM feature", response);
+        return response;
+    }
+    
+    private static void handleValidationError(String message, JSONObject response, Client clientObject) {
+        response = XUtils.buildJsonErrorResponse(Requests.SERVER_BAD_REQUEST, Events.EVENT_TYPE, Events.VALIDATION_ERROR_EVENT, message, response);
+        broadcast(clientObject, response.toString());
+        
+    }
+    
+    private static void handleAuthenticationError(String message, JSONObject response, Client clientObject) {
+        response = XUtils.buildJsonErrorResponse(Requests.SERVER_BAD_REQUEST, Events.EVENT_TYPE, "authentication", message, response);
         broadcast(clientObject, response.toString());
     }
 
