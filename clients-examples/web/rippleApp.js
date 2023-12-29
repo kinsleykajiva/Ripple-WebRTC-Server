59aa74b112787ae1c3da0be8c7e1c3c72c89225a
kinsleykajiva <kajivakinsley@gmail.com>
2023-12-29 20:29:26 +0200

    "Added custom log function to RippleSDK with enhanced features" and socket  functions

--- clients-examples/web/rippleApp.js
+++ clients-examples/web/rippleApp.js
@@ -11,6 +11,67 @@ const RippleSDK = {
         hasAccessToVideoPermission  : false,
         hasAccessToAudioPermission  : false,
         maxRetries: 10,
+        remindServerTimeoutInSeconds: 26 ,
+        reminderInterval  : null,
+        startToRemindServerOfMe:()=>{
+            RippleSDK.utils.log('startToRemindServerOfMe');
+            RippleSDK.app.reminderInterval = setInterval(()=>{
+                const body =  {
+                    clientID: RippleSDK.serverClientId,
+                    requestType: '',
+                };
+            }, RippleSDK.app.remindServerTimeoutInSeconds *1000);
+
+        },
+    },
+    transports:{
+        websocket: {
+            socket: null,
+            isConnected: false,
+            webSocketSendAction: (messageObject) => {
+                if(!RippleSDK.transports.websocket.isConnected){
+                    RippleSDK.utils.log('webSocketSendAction', 'not connected');
+                    return;
+                }
+                if(!messageObject){
+                    RippleSDK.utils.log('webSocketSendAction', 'no messageObject');
+                    return;
+                }
+                if(RippleSDK.transports.websocket.socket.readyState !== 1){
+                    RippleSDK.utils.log('webSocketSendAction', 'not ready');
+                    return;
+                }else{
+                    RippleSDK.utils.log('webSocketSendAction', 'ready');
+                    RippleSDK.transports.websocket.socket.send(JSON.stringify(messageObject));
+                }
+
+            },
+            connect: () => {
+                RippleSDK.transports.websocket.socket = new WebSocket(RippleSDK.serverUrl);
+                RippleSDK.transports.websocket.socket.onopen = () => {
+                    RippleSDK.transports.websocket.isConnected = true;
+                    RippleSDK.transports.websocket.socket.send(JSON.stringify({
+                        clientID: RippleSDK.clientID,
+                        requestType: 'connect',
+                    }));
+                };
+                RippleSDK.transports.websocket.socket.onmessage = (event) => {
+                    const data = JSON.parse(event.data);
+                    RippleSDK.utils.log('onmessage', data);
+                    if (data.requestType === 'connect') {
+                        RippleSDK.utils.log('onmessage', 'connected');
+                        RippleSDK.app.startToRemindServerOfMe();
+                    }
+                };
+                RippleSDK.transports.websocket.socket.onclose = () => {
+                    RippleSDK.transports.websocket.isConnected = false;
+                    RippleSDK.utils.log('onclose');
+                };
+                RippleSDK.transports.websocket.socket.onerror = (error) => {
+                    RippleSDK.utils.log('onerror', error);
+                };
+            },
+        }
     },
     utils             : {
         log               : console.log.bind(console),
@@ -20,6 +81,23 @@ const RippleSDK = {
         info              : console.info.bind(console),
         trace             : console.trace.bind(console),
         assert            : console.assert.bind(console),
+        isWebRTCSupported               : () => !!window.RTCPeerConnection,
+        replaceAll: (f, r) => this.split(f).join(r),
+        uniqueIDGenerator: (seed = '', maxSize = 22) => {
+            const alphabet       = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+            const date            = new Date();
+            const timeString     = `${date.getHours()}${date.getMinutes()}${date.getSeconds()}`.padStart(6, '0');
+            const dateTimeString = `${seed}${timeString}${Math.random().toString(36).substr(2, 4)}`.slice(0, 12);
+            let uniID            = '';
+            for (let i = 0; i < maxSize; i++) {
+                uniID += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+            }
+            for (let i = 0; i < dateTimeString.length; i++) {
+                const index = parseInt(dateTimeString.charAt(i), 36);
+                uniID = uniID.substr(0, index) + dateTimeString.charAt(i) + uniID.substr(index + 1);
+            }
+            return uniID;
+        },
     },
     init              : {},
     notificationsTypes: {},
