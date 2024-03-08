90ffc481aecc84333e52514f09f73743fa6a2d18
kinsleykajiva <kajivakinsley@gmail.com>
2024-03-08 11:37:20 +0200

    Refactor code for readability and consistency
    Whitespace and indentation have been standardized to improve readability and consistency. This refactor doesn't change any functionality of the code. The update helps to keep the code clean and maintainable, ensuring better collaboration among team members.

--- src/main/java/africa/jopen/ripple/sockets/WebRTCGStreamerPlugInEventsHandler.java
+++ src/main/java/africa/jopen/ripple/sockets/WebRTCGStreamerPlugInEventsHandler.java
@@ -12,189 +12,192 @@ import java.io.File;
 import java.util.Objects;
 
 public class WebRTCGStreamerPlugInEventsHandler {
-	static  Logger log = Logger.getLogger(WebRTCGStreamerPlugInEventsHandler.class.getName());
+	static Logger log = Logger.getLogger(WebRTCGStreamerPlugInEventsHandler.class.getName());
 	
 	
-	 static void answer( WsSession session ,Client client,
-	                      JSONObject jsonObject, String transaction, boolean last) {
-		 if (!jsonObject.has("answer")) {
-			 log.info("Required answer not found,Invalid request");
-			 session.send(
-					 new JSONObject()
-							 .put("success", false)
-							 .put("eventType", jsonObject.getString("requestType"))
-							 .put("accessAuth", "GENERAL")
-							 .put("message", "Required answer not found,Invalid request")
-							 .put("transaction", transaction)
-							 .put("clientID", jsonObject.getString("clientID"))
-							 .toString()
-					 , last);
-			 
-			 return;
-		 }
-		 
-		 
-		 if (!jsonObject.has("threadRef")) {
-			 log.info("Required threadRef not found,Invalid request");
-			 session.send(
-					 new JSONObject()
-							 .put("success", false)
-							 .put("eventType", jsonObject.getString("requestType"))
-							 .put("accessAuth", "GENERAL")
-							 .put("message", "Required threadRef not found,Invalid request")
-							 .put("transaction", transaction)
-							 .put("clientID", jsonObject.getString("clientID"))
-							 .toString()
-					 , last);
-			 
-			 return;
-		 }
-		 var plugin = client.getWebRTCStreamMap().get(jsonObject.getInt("threadRef"));
-		 
-		 if (Objects.isNull(plugin)) {
-			 session.send(
-					 new JSONObject()
-							 .put("success", false)
-							 .put("eventType", jsonObject.getString("requestType"))
-							 .put("accessAuth", "GENERAL")
-							 .put("message", "Required threadRef Value not found,Reference error")
-							 .put("transaction", transaction)
-							 .put("clientID", jsonObject.getString("clientID"))
-							 .toString()
-					 , last);
-			 return;
-		 }
-		 plugin.setTransaction(transaction);
-		 plugin.handleSdp(jsonObject.getString("answer"));
-	 }
+	static void answer( WsSession session, Client client,
+	                    JSONObject jsonObject, String transaction, boolean last ) {
+		if (!jsonObject.has("answer")) {
+			log.info("Required answer not found,Invalid request");
+			session.send(
+					new JSONObject()
+							.put("success", false)
+							.put("eventType", jsonObject.getString("requestType"))
+							.put("accessAuth", "GENERAL")
+							.put("message", "Required answer not found,Invalid request")
+							.put("transaction", transaction)
+							.put("clientID", jsonObject.getString("clientID"))
+							.toString()
+					, last);
+			
+			return;
+		}
+		
+		
+		if (!jsonObject.has("threadRef")) {
+			log.info("Required threadRef not found,Invalid request");
+			session.send(
+					new JSONObject()
+							.put("success", false)
+							.put("eventType", jsonObject.getString("requestType"))
+							.put("accessAuth", "GENERAL")
+							.put("message", "Required threadRef not found,Invalid request")
+							.put("transaction", transaction)
+							.put("clientID", jsonObject.getString("clientID"))
+							.toString()
+					, last);
+			
+			return;
+		}
+		var plugin = client.getWebRTCStreamMap().get(jsonObject.getInt("threadRef"));
+		
+		if (Objects.isNull(plugin)) {
+			session.send(
+					new JSONObject()
+							.put("success", false)
+							.put("eventType", jsonObject.getString("requestType"))
+							.put("accessAuth", "GENERAL")
+							.put("message", "Required threadRef Value not found,Reference error")
+							.put("transaction", transaction)
+							.put("clientID", jsonObject.getString("clientID"))
+							.toString()
+					, last);
+			return;
+		}
+		plugin.setTransaction(transaction);
+		plugin.handleSdp(jsonObject.getString("answer"));
+	}
 	
-	 static void iceCandidate( WsSession session ,Client client,
-	                      JSONObject jsonObject, String transaction, boolean last) {
-		 if (!jsonObject.has("candidate")) {
-			 log.info("Required candidate not found,Invalid request");
-			 session.send(
-					 new JSONObject()
-							 .put("success", false)
-							 .put("eventType", jsonObject.getString("requestType"))
-							 .put("accessAuth", "GENERAL")
-							 .put("message", "Required candidate not found,Invalid request")
-							 .put("transaction", transaction)
-							 .put("clientID", jsonObject.getString("clientID"))
-							 .toString()
-					 , last);
-			 
-			 return;
-		 }
-		 if (!jsonObject.has("sdpMLineIndex")) {
-			 log.info("Required sdpMLineIndex not found,Invalid request");
-			 session.send(
-					 new JSONObject()
-							 .put("success", false)
-							 .put("eventType", jsonObject.getString("requestType"))
-							 .put("accessAuth", "GENERAL")
-							 .put("message", "Required sdpMLineIndex not found,Invalid request")
-							 .put("transaction", transaction)
-							 .put("clientID", jsonObject.getString("clientID"))
-							 .toString()
-					 , last);
-			 
-			 return;
-		 }
-		 
-		 if (!jsonObject.has("threadRef")) {
-			 log.info("Required threadRef not found,Invalid request");
-			 session.send(
-					 new JSONObject()
-							 .put("success", false)
-							 .put("eventType", jsonObject.getString("requestType"))
-							 .put("accessAuth", "GENERAL")
-							 .put("message", "Required threadRef not found,Invalid request")
-							 .put("transaction", transaction)
-							 .put("clientID", jsonObject.getString("clientID"))
-							 .toString()
-					 , last);
-			 
-			 return;
-		 }
-		 var plugin = client.getWebRTCStreamMap().get(jsonObject.getInt("threadRef"));
-		 if (Objects.isNull(plugin)) {
-			 session.send(
-					 new JSONObject()
-							 .put("success", false)
-							 .put("eventType", jsonObject.getString("requestType"))
-							 .put("accessAuth", "GENERAL")
-							 .put("message", "Required threadRef Value not found,Reference error")
-							 .put("transaction", transaction)
-							 .put("clientID", jsonObject.getString("clientID"))
-							 .toString()
-					 , last);
-			 return;
-		 }
-		 plugin.setTransaction(transaction);
-		 plugin.handleIceSdp(jsonObject.getString("candidate"), jsonObject.getInt("sdpMLineIndex"));
-		 
-	 }
-	 static void newThread( WsSession session ,Client client,
-	                      JSONObject jsonObject, String transaction, boolean last) {
-		 
-		 int thread = 0;
-		 JSONObject reqstData = jsonObject.getJSONObject("data");
-		 if (jsonObject.getString("feature").equals("G_STREAM_BROADCASTER")) {
-			 
-			 var filePath = XUtils.MAIN_CONFIG_MODEL.storagePath().media() + reqstData.getString("file");
-			 
-			 if (!new File(filePath).exists()) {
-				 log.info("File not found: " + filePath);
-				 
-				 client.replyToNewThreadInvalidRequest(transaction, -1);
-				 return;
-				 
-			 }
-			 thread = client.createAccessGStreamerPlugIn(
-					 new MediaFile(filePath
-							 , 0));
-			 
-		 }
-		 client.replyToNewThreadRequest(transaction, thread,jsonObject.getString("feature"));
-	 }
-	 static void startBroadCast( WsSession session ,Client client,
-	                      JSONObject jsonObject, String transaction, boolean last) {
-		 if (!jsonObject.has("threadRef")) {
-			 log.info("Required threadRef not found,Invalid request");
-			 session.send(
-					 new JSONObject()
-							 .put("success", false)
-							 .put("eventType", jsonObject.getString("requestType"))
-							 .put("accessAuth", "GENERAL")
-							 .put("message", "Required threadRef not found,Invalid request")
-							 .put("transaction", transaction)
-							 .put("clientID", jsonObject.getString("clientID"))
-							 .toString()
-					 , last);
-			 
-			 return;
-		 }
-		 var plugin = client.getWebRTCStreamMap().get(jsonObject.getInt("threadRef"));
-		 if (Objects.isNull(plugin)) {
-			 session.send(
-					 new JSONObject()
-							 .put("success", false)
-							 .put("eventType", jsonObject.getString("requestType"))
-							 .put("accessAuth", "GENERAL")
-							 .put("message", "Required threadRef Value not found,Reference error")
-							 .put("transaction", transaction)
-							 .put("clientID", jsonObject.getString("clientID"))
-							 .toString()
-					 , last);
-			 return;
-		 }
-		 plugin.setTransaction(transaction);
-		 plugin.startCall();
-	 }
-	 static void resume( WsSession session ,Client client,
-	                      JSONObject jsonObject, String transaction, boolean last) {
-		 
-		 
+	static void iceCandidate( WsSession session, Client client,
+	                          JSONObject jsonObject, String transaction, boolean last ) {
+		if (!jsonObject.has("candidate")) {
+			log.info("Required candidate not found,Invalid request");
+			session.send(
+					new JSONObject()
+							.put("success", false)
+							.put("eventType", jsonObject.getString("requestType"))
+							.put("accessAuth", "GENERAL")
+							.put("message", "Required candidate not found,Invalid request")
+							.put("transaction", transaction)
+							.put("clientID", jsonObject.getString("clientID"))
+							.toString()
+					, last);
+			
+			return;
+		}
+		if (!jsonObject.has("sdpMLineIndex")) {
+			log.info("Required sdpMLineIndex not found,Invalid request");
+			session.send(
+					new JSONObject()
+							.put("success", false)
+							.put("eventType", jsonObject.getString("requestType"))
+							.put("accessAuth", "GENERAL")
+							.put("message", "Required sdpMLineIndex not found,Invalid request")
+							.put("transaction", transaction)
+							.put("clientID", jsonObject.getString("clientID"))
+							.toString()
+					, last);
+			
+			return;
+		}
+		
+		if (!jsonObject.has("threadRef")) {
+			log.info("Required threadRef not found,Invalid request");
+			session.send(
+					new JSONObject()
+							.put("success", false)
+							.put("eventType", jsonObject.getString("requestType"))
+							.put("accessAuth", "GENERAL")
+							.put("message", "Required threadRef not found,Invalid request")
+							.put("transaction", transaction)
+							.put("clientID", jsonObject.getString("clientID"))
+							.toString()
+					, last);
+			
+			return;
+		}
+		var plugin = client.getWebRTCStreamMap().get(jsonObject.getInt("threadRef"));
+		if (Objects.isNull(plugin)) {
+			session.send(
+					new JSONObject()
+							.put("success", false)
+							.put("eventType", jsonObject.getString("requestType"))
+							.put("accessAuth", "GENERAL")
+							.put("message", "Required threadRef Value not found,Reference error")
+							.put("transaction", transaction)
+							.put("clientID", jsonObject.getString("clientID"))
+							.toString()
+					, last);
+			return;
+		}
+		plugin.setTransaction(transaction);
+		plugin.handleIceSdp(jsonObject.getString("candidate"), jsonObject.getInt("sdpMLineIndex"));
+		
+	}
+	
+	static void newThread( WsSession session, Client client,
+	                       JSONObject jsonObject, String transaction, boolean last ) {
+		
+		int        thread    = 0;
+		JSONObject reqstData = jsonObject.getJSONObject("data");
+		if (jsonObject.getString("feature").equals("G_STREAM_BROADCASTER")) {
+			
+			var filePath = XUtils.MAIN_CONFIG_MODEL.storagePath().media() + reqstData.getString("file");
+			
+			if (!new File(filePath).exists()) {
+				log.info("File not found: " + filePath);
+				
+				client.replyToNewThreadInvalidRequest(transaction, -1);
+				return;
+				
+			}
+			thread = client.createAccessGStreamerPlugIn(
+					new MediaFile(filePath
+							, 0));
+			
+		}
+		client.replyToNewThreadRequest(transaction, thread, jsonObject.getString("feature"));
+	}
+	
+	static void startBroadCast( WsSession session, Client client,
+	                            JSONObject jsonObject, String transaction, boolean last ) {
+		if (!jsonObject.has("threadRef")) {
+			log.info("Required threadRef not found,Invalid request");
+			session.send(
+					new JSONObject()
+							.put("success", false)
+							.put("eventType", jsonObject.getString("requestType"))
+							.put("accessAuth", "GENERAL")
+							.put("message", "Required threadRef not found,Invalid request")
+							.put("transaction", transaction)
+							.put("clientID", jsonObject.getString("clientID"))
+							.toString()
+					, last);
+			
+			return;
+		}
+		var plugin = client.getWebRTCStreamMap().get(jsonObject.getInt("threadRef"));
+		if (Objects.isNull(plugin)) {
+			session.send(
+					new JSONObject()
+							.put("success", false)
+							.put("eventType", jsonObject.getString("requestType"))
+							.put("accessAuth", "GENERAL")
+							.put("message", "Required threadRef Value not found,Reference error")
+							.put("transaction", transaction)
+							.put("clientID", jsonObject.getString("clientID"))
+							.toString()
+					, last);
+			return;
+		}
+		plugin.setTransaction(transaction);
+		plugin.startCall();
+	}
+	
+	static void resume( WsSession session, Client client,
+	                    JSONObject jsonObject, String transaction, boolean last ) {
+		
+		
 		if (!jsonObject.has("threadRef")) {
 			log.info("Required threadRef not found,Invalid request");
 			session.send(
@@ -227,8 +230,9 @@ public class WebRTCGStreamerPlugInEventsHandler {
 		plugin.setTransaction(transaction);
 		plugin.resumeTransmission();
 	}
-	 static void decreaseVolume( WsSession session ,Client client,
-	                      JSONObject jsonObject, String transaction, boolean last) {
+	
+	static void decreaseVolume( WsSession session, Client client,
+	                            JSONObject jsonObject, String transaction, boolean last ) {
 		if (!jsonObject.has("threadRef")) {
 			log.info("Required threadRef not found,Invalid request");
 			session.send(
@@ -261,8 +265,9 @@ public class WebRTCGStreamerPlugInEventsHandler {
 		plugin.setTransaction(transaction);
 		plugin.decreaseVolume();
 	}
-	 static void increaseVolume( WsSession session ,Client client,
-	                      JSONObject jsonObject, String transaction, boolean last) {
+	
+	static void increaseVolume( WsSession session, Client client,
+	                            JSONObject jsonObject, String transaction, boolean last ) {
 		if (!jsonObject.has("threadRef")) {
 			log.info("Required threadRef not found,Invalid request");
 			session.send(
@@ -296,45 +301,40 @@ public class WebRTCGStreamerPlugInEventsHandler {
 		plugin.increaseVolume();
 	}
 	
- static void pause( WsSession session ,Client client,
-	                      JSONObject jsonObject, String transaction, boolean last) {
-	if (!jsonObject.has("threadRef")) {
-		log.info("Required threadRef not found,Invalid request");
-		session.send(
-				new JSONObject()
-						.put("success", false)
-						.put("eventType", jsonObject.getString("requestType"))
-						.put("accessAuth", "GENERAL")
-						.put("message", "Required threadRef not found,Invalid request")
-						.put("transaction", transaction)
-						.put("clientID", jsonObject.getString("clientID"))
-						.toString()
-				, last);
-		
-		return;
-	}
+	static void pause( WsSession session, Client client,
+	                   JSONObject jsonObject, String transaction, boolean last ) {
+		if (!jsonObject.has("threadRef")) {
+			log.info("Required threadRef not found,Invalid request");
+			session.send(
+					new JSONObject()
+							.put("success", false)
+							.put("eventType", jsonObject.getString("requestType"))
+							.put("accessAuth", "GENERAL")
+							.put("message", "Required threadRef not found,Invalid request")
+							.put("transaction", transaction)
+							.put("clientID", jsonObject.getString("clientID"))
+							.toString()
+					, last);
+			
+			return;
+		}
 		WebRTCGStreamerPlugIn plugin = client.getWebRTCStreamMap().get(jsonObject.getInt("threadRef"));
-	if (Objects.isNull(plugin)) {
-		session.send(
-				new JSONObject()
-						.put("success", false)
-						.put("eventType", jsonObject.getString("requestType"))
-						.put("accessAuth", "GENERAL")
-						.put("message", "Required threadRef Value not found,Reference error")
-						.put("transaction", transaction)
-						.put("clientID", jsonObject.getString("clientID"))
-						.toString()
-				, last);
-		return;
+		if (Objects.isNull(plugin)) {
+			session.send(
+					new JSONObject()
+							.put("success", false)
+							.put("eventType", jsonObject.getString("requestType"))
+							.put("accessAuth", "GENERAL")
+							.put("message", "Required threadRef Value not found,Reference error")
+							.put("transaction", transaction)
+							.put("clientID", jsonObject.getString("clientID"))
+							.toString()
+					, last);
+			return;
+		}
+		plugin.setTransaction(transaction);
+		plugin.pauseTransmission();
 	}
-	plugin.setTransaction(transaction);
-	plugin.pauseTransmission();
-}
-
-
-
-
-
-
-
+	
+	
 }
